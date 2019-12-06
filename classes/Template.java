e6d793e63198ef86f8ded7ddc456049c2a2feaaf
ruipang1122 <ruipang1122@gmail.com>
2019-12-05 22:19:12 -0500

    template added

--- classes/Template.java
+++ classes/Template.java
@@ -1,3 +1,23 @@
-public class Template {
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class Template {
+    private String templateName;
+    private List<Category> categoryList;
+
+    public Template(String templateName) {
+        this.templateName = templateName;
+        categoryList = new ArrayList<>();
+    }
+
+    public String getTemplateName() { return templateName;}
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public void addCategory (Category cat) {
+        categoryList.add(cat);
+    }
 }
